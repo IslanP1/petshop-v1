cc8dcda6a2405fe8468d79b6572b70eeedd04cc0
IslanP1 <islanpereiradeoliveira@gmail.com>
2025-01-23 10:05:57 -0300

    feature: adição midlewares essenciais e correções de alguns bugs]

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 import { v4 as uuid } from "uuid";
 import cors from "cors";
+import { error } from "console";
 
 let petShops: PetShop[] = [];
 
@@ -9,8 +10,54 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+// Middleware para verificar se o petshop existe
+function checkExistsUserAccount(req: Request, res: Response, next: NextFunction) {
+    const cnpj = req.body.cnpj as string;
+    console.log(cnpj);
+
+    const petshop = petShops.find((petshop) => petshop.cnpj === cnpj);
+
+    if (!petshop) {
+        res.status(404).json({ error: "user not exists" });
+        return;
+    }
+
+    // Adiciona o petshop no objeto da requisição para usar na rota
+    req.petshop = petshop;
+    next();
+}
+
+function checkUsersEquals(req: Request, res: Response, next: NextFunction) {
+    const cnpj = req.body.cnpj;
+    const verificaPetshop = petShops.find((petshop) => petshop.cnpj === cnpj);
+
+    if (verificaPetshop) {
+        res.status(400).json({ message: "Já existe esse petshop! " });
+        return;
+    }
+    
+    req.petshop = req.body;
+    next();
+}
+
+
+function validarCNPJ(req: Request, res: Response, next: NextFunction) {
+    const cnpj = req.body.cnpj;
+    console.log(cnpj)
+    const regex =  /^\d{2}\.\d{3}\.\d{3}\/0001-\d{2}$/;
+    const validacao = regex.test(cnpj);
+
+    if (validacao) {
+        req.body.cnpj = cnpj;
+        next();
+    }
+
+    res.status(400).json({ error: "CNPJ inválido!" })
+    return;
+}
+
 //criar Petshop
-server.post("/petshops", (req: Request, res: Response) => {
+server.post("/petshops", checkUsersEquals, validarCNPJ, (req: Request, res: Response) => {
     const data = req.body as PetShop;
 
     const petshop: PetShop = {
@@ -21,7 +68,8 @@ server.post("/petshops", (req: Request, res: Response) => {
     };
 
     petShops.push(petshop);
-    res.status(201).json({ message: "Petshop criado com sucesso!" });
+    res.status(201).json({ petshop})
+    return;
 });
 
 //criar pet
@@ -66,7 +114,35 @@ server.get("/petshop/:id/pets", (req: Request, res: Response) => {
     } else {
         res.status(404).json({ message: "Petshop não encontrado!" });
     };
-}) 
+});
+
+//atualizar petshop
+server.put("/petshop/:id", (req: Request, res: Response) => {
+    const petshopId = req.params.id;
+
+    const petshopIndex = petShops.find((petshop) => petshop.id === petshopId);
+
+    if (petshopIndex) {
+        petshopIndex.name = req.body.name;
+        petshopIndex.cnpj = req.body.cnpj;
+        res.status(200).json({ message: "Petshop atualizado com sucesso!" });
+    } else {
+        res.status(404).json({ message: "Petshop não encontrado!" });
+    };
+});
+
+//atualizar pet
+server.put("/petshop/:id/pet/:id-pet", (req: Request, res: Response) => {
+    const petshopId = req.params.id;
+
+
+
+
+
+
+})
+
+
 
 server.listen("3000", () =>
     console.log("server online on port 3000")
